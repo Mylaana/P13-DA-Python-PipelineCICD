73e651cefdc2c96ce3cc5b4b8e810e7e71d1b7ad
Mylaana <alexandre.thore34@gmail.com>
2024-01-30 16:19:31 +0100

    update afterinstall

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -3,3 +3,7 @@ cd /home/apps
 source env/bin/activate
 pip install -r requirements.txt
 deactivate
+
+#printing instance dns in file
+result=$( aws ec2 describe-instances --instance-ids i-04addbebb4c86a95e --query 'Reservations[].Instances[].PublicDnsName' | sed 's/[\["\]//g; s/]$//')
+echo ALLOWED_HOST = $result > test_log.txt
