362d3b7de5452a0840b793dadcbc69185bfcc928
Mylaana <alexandre.thore34@gmail.com>
2024-01-31 14:10:33 +0100

    add dns writing afterinstall

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 deactivate
 
 #printing instance dns in file
-result=$( aws ec2 describe-instances --instance-ids i-04addbebb4c86a95e --query 'Reservations[].Instances[].PublicDnsName' | sed 's/[\["\]//g; s/]$//')
-echo ALLOWED_HOST = $result > test_log.txt
+result=$( aws ec2 describe-instances --instance-ids i-04addbebb4c86a95e --query 'Reservations[].Instances[].PublicDnsName' | sed 's/[\["\]//g; s/]$//' | tr -d '\n' | sed 's/^[ \t]*//;s/[ \t]*$//')
+sudo sed -i 's/ec2_dns/'"${result}"'/g' setup.cfg
