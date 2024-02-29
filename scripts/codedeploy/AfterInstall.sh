e67b895d23ac83ee436b4c29662727f7e49d88b6
Mylaana <alexandre.thore34@gmail.com>
2024-02-29 13:34:15 +0100

    deactivate docker-compose

--- scripts/codedeploy/AfterInstall.sh
+++ scripts/codedeploy/AfterInstall.sh
@@ -2,4 +2,4 @@
 
 # building and running containers
 cd /home/ubuntu/oc_lettings_site
-sudo docker-compose -f docker-compose-deploy.yml -d --build
+# sudo docker-compose -f docker-compose-deploy.yml -d --build
