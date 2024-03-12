8005a48c09ff0416b80e5f29025ac83cecc380d8
Mylaana <alexandre.thore34@gmail.com>
2024-03-12 13:15:49 +0100

    added templates and debug error triggering endpoint

--- app/oc_lettings_site/urls.py
+++ app/oc_lettings_site/urls.py
@@ -5,6 +5,10 @@ import oc_lettings_site.views
 import lettings.views
 import profiles.views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('', oc_lettings_site.views.index, name='index'),
     path('lettings/', lettings.views.lettings_index, name='lettings_index'),
@@ -12,4 +16,5 @@ urlpatterns = [
     path('profiles/', profiles.views.profiles_index, name='profiles_index'),
     path('profiles/<str:username>/', profiles.views.profile, name='profile'),
     path('admin/', admin.site.urls),
+    path('sentry-debug/', trigger_error),
 ]
