a814b92b372fc21b93bc45cf181425a52c1c964e
Mylaana <alexandre.thore34@gmail.com>
2024-03-12 12:17:59 +0100

    add 404 template

--- app/oc_lettings_site/views.py
+++ app/oc_lettings_site/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-"""from .models import Letting, Profile"""
-
 
 # Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie quam lobortis leo consectetur ullamcorper non id est. Praesent dictum, nulla eget feugiat sagittis, sem mi convallis eros,
 # vitae dapibus nisi lorem dapibus sem. Maecenas pharetra purus ipsum, eget consequat ipsum lobortis quis. Phasellus eleifend ex auctor venenatis tempus.
